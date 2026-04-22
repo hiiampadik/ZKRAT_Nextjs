55758559ded7adc9275a5dbb34dae6efa029c904
Bronislav Musil <brona.musil@gmail.com>
2026-04-22 16:41:54 +0200

    Update build command in deploy.yml and set output directory for sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://zkratkolektiv.com",
   generateRobotsTxt: true,
   generateIndexSitemap: true,
+  outDir: process.env.GITHUB_PAGES === "true" ? "./out" : "./public",
 };
